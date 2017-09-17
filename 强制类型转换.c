4979f4b80b98830eb20437d989d3e30c41c3e5b5
weizhaopeng <WZP_1995@163.com>
2017-09-17 02:37:21 -0700

    add 强制类型转换

---
+++ 强制类型转换.c
@@ -0,0 +1,14 @@
+#include"stdio.h"
+int main(void)
+{
+int a=-10;
+char b='s';
+double c=12.56778;
+long double d=25.787766;
+float e=125.67677;
+unsigned int f=20;
+int g;
+g=(a+b)+(c-d)+(e+f);
+printf("%lf",g);
+return 0;
+} 
