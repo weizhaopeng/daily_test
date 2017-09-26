ee9db41005080545b8d58bd7dbbb8148fc8d941f
weizhaopeng <WZP_1995@163.com>
2017-09-26 08:08:07 -0700

    右移运算

--- yunsuan.c
+++ yunsuan.c
@@ -1,8 +1,21 @@
 #include"stdio.h"
+#include"stdlib.h"
 int main(void)
 {
-int n=15;
+char a;
+int n=15,x=15,y=-15,z=16,w;
+//itoa(z,w,2);
+printf("请输入五个字符\n");
+/*for(w=0;w<5;w++)
+getchar(*(a+w));
+for(w=0;w<5;w++)
+putchar(*(a+w));*/
+a=getchar();
+putchar(a);
+putchar('\n');
 printf("bianhuanhou=%d\n",~n);
 printf("luoji=%d\n",n&10);
+printf("右移+=%d\n右移-=%d\n",x>>=2,y>>=2);
+printf("z=%d\n",sizeof(int));
 return 0;
 }
