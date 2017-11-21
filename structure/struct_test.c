586be782480b5a4312d849443da2c8bb524b55d0
weizhaopeng <WZP_1995@163.com>
2017-11-21 07:39:57 -0800

    链表的创建出现问题，输入的地方和打印缺少性别

--- structure/struct_test.c
+++ structure/struct_test.c
@@ -1,6 +1,8 @@
 #include"stdio.h"
 #include"stdlib.h"
+
 static int count;	
+
 struct stu
 {
 	int num;
@@ -21,41 +23,40 @@ NODE *creat()
 	NODE *head,*new,*tail;
 	head=new=tail=(NODE *)malloc(sizeof(NODE));
 	tail->next=NULL;
-	count++;
+	puts("请输入学生的学号，姓名，性别：\n例如：15011630 魏朝鹏 m\n(男=m，女=f)");
+	scanf("%d%s%c",&new->student.num,new->student.name,&new->student.sex);
+
 	for(int i=1;i<count;i++)
 	{
 		new=(NODE *)malloc(sizeof(NODE));
-		if(new=NULL)
+
+	/*	if(new==NULL)
 		{	
 			puts("分配错误");
 			exit(-1);
 		}
+*/
 		tail->next=new;
-		new->next=NULL;
-		tail=new;
-
 		puts("请输入学生的学号，姓名，性别：\n例如：15011630 魏朝鹏 m\n(男=m，女=f)");
 		scanf("%d%s%c",&new->student.num,new->student.name,&new->student.sex);
-		count++;
+		
+		tail=new;
+		tail->next=NULL;
 	}
 	return head;
 }	
 int main (void)
 {
 	int n;
-	STU wzp={15011630,"weizhaopeng",'f'};
 	NODE *head,*temp;
-	printf("%s\n",wzp.name);
 
 	puts("请输入学生人数");
-	scanf("%d",&n);
-	count=n;
+	scanf("%d",&count);
 
-	head=creat();
+	temp=head=creat();
 	
-	for(int i=0;i<count;i++)
+	for(int i=0;i<count&&temp->next!=NULL;i++)
 	{
-		temp=head;
 		printf("%d\t%s\t%c\n",temp->student.num,temp->student.name,temp->student.sex);
 		temp=temp->next;
 	}	
