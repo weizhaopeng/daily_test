9eece311f8daa98bd6564b43ea325832d667c0f3
weizhaopeng <WZP_1995@163.com>
2017-12-22 07:45:52 -0800

    测试printf\033[结束，部分测试不出来再议

--- pretreat/pretreat_test.c
+++ pretreat/pretreat_test.c
@@ -1,16 +1,12 @@
 #include"pretreat.h"
 
 #define MAX 50
+#define ADD(a,b) (a+b)
 void main (void)
 {
 	int a=MAX;
 	int b=MAX+1;
-	//printf("\033[47;36ma+b=%d\n\033[8m",a+b);
-	printf("\033[32m我叫魏\033[7m朝鹏\033[2J之后呢");
-	printf("\033[3;4H我叫杨晓涵\n");
 	printf("\033[40;32ma+b=%d\n\033[0m",a+b);
-	printf("\033[1A123\n\033[3B456\033[0m");
-	return ;
-	
+	printf("\033[41;30ma+b=%d\n\033[0m",ADD(a,b));
 }
 
