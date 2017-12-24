a9c12c166baf94ecdf9b7ba3aad5a373c289a828
weizhaopeng <WZP_1995@163.com>
2017-12-24 08:25:41 -0800

    预处理进行到#line的测试，出现问题

--- pretreat/pretreat_test.c
+++ pretreat/pretreat_test.c
@@ -1,12 +1,9 @@
 #include"pretreat.h"
-
-#define MAX 50
-#define ADD(a,b) (a+b)
+#line 4
+#define _LINE_
 void main (void)
 {
-	int a=MAX;
-	int b=MAX+1;
-	printf("\033[40;32ma+b=%d\n\033[0m",a+b);
-	printf("\033[41;30ma+b=%d\n\033[0m",ADD(a,b));
-}
+	int a=_LINE_;
+	printf("%d\n",a);
 
+}
