c31c890bc2be00030fc08f003c4615133fefcedd
weizhaopeng <WZP_1995@163.com>
2018-11-05 21:49:12 +0800

    完成了线程同步的测试，但是在测试线程pthread_mutex_timedlock的时候，我们让线程3trylock失败退出，让线程1锁住数据，睡眠10s，但是timedlock指定了只有5s等待，所以，线程2应该返回才对，但是程序彻底卡死了

--- zhou_process/thread/thread.h
+++ zhou_process/thread/thread.h
@@ -7,6 +7,14 @@
 #include <errno.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <time.h>
+
+//数组加入互斥量
+typedef struct array_t {
+	int *array;
+	int length;
+	pthread_mutex_t mutex;
+} array_t;
 
 #define LENGTH_ARRAY 20
 #endif
--- zhou_process/thread/thread_syn.c
+++ zhou_process/thread/thread_syn.c
@@ -3,80 +3,174 @@
  *后面的线程进行等待，查看相应的数据的值
  */
 #include "thread.h"
-//测试结构体是否可以定义的时候初始化
-typedef struct array_t {
-	int array[100];
-	int length;
-	pthread_mutex_t mutex;
-} array_t;
-
+		
 void *function(void *argv);
+void *function1(void *argv);
+static inline int get_length  (const void *array_p);
+static inline int set_elements(const void *array_p, void *(*oper_elements)(void *), void *argv);
+static inline int get_element (const void *array_p, const int index);
+static inline int set_element (const void *array_p, const int index, int new_value);
 
 int main(void) {
-	array_t at;
-	int		ret = 0;
+	int 	  ret = 0;
+	array_t  *at = NULL;
+	pthread_t th1, th2, th3;
+	struct timespec time_wait = {
+		.tv_sec = 5,
+		.tv_nsec = 0
+	};
+
+	at 		  = calloc(1, sizeof(array_t));
+	at->array = calloc(1, sizeof(int)*100);
+	at->length = 0;
+	pthread_mutex_init(&at->mutex, NULL);
+	ret = pthread_mutex_timedlock(&at->mutex, &time_wait);
+	if (ret != 0) {
+		printf("timedlock:%s\n", strerror(ret));
+		return (-1);
+	}
+
 	//初始化数组
 	puts("初始化数组内容:");
 	for (int i = 0; i < LENGTH_ARRAY; i++) {
-		at.array[i] = 1+(int)random()%100;
-		printf("%d ", at.array[i]);
+		at->array[i] = 1+(int)random()%100;
+		printf("%d ", at->array[i]);
 	}
 	puts("");
-	at.length = LENGTH_ARRAY;
-	at.mutex = PTHREAD_MUTEX_INITIALIZER;
-	//pthread_mutex_init(&at.mutex, NULL);
+	at->length = LENGTH_ARRAY;
 	
 	//创建线程
-	pthread_t th1, th2;
-	ret = pthread_create(&th1, NULL, function, NULL);
-	if (ret != 0){
-		strerror(ret);
-		return -1;
-	}
+	pthread_create(&th1, NULL, function, at);
 	puts("线程1创建成功");
-
-	ret = pthread_create(&th2, NULL, function, NULL);
-	if (ret != 0){
-		strerror(ret);
-		return -1;
-	}
+	pthread_create(&th2, NULL, function, at);
 	puts("线程2创建成功");
+	pthread_create(&th3, NULL, function1, at);
+	puts("线程3创建成功");
+
 	pthread_join(th1, NULL);
 	pthread_join(th2, NULL);
+	pthread_join(th3, NULL);
 
+	puts("初始化数组内容:");
+	for (int i = 0; i < LENGTH_ARRAY; i++)
+		printf("%d ", at->array[i]);
+	puts("");
+
+	pthread_mutex_destroy(&at->mutex);
+	free(at->array); free(at);
 	return 0;
 }
 
 void *function(void *argv) {
-	array_t *at = (array_t*)argv;
+	array_t *at = argv;
 	int		 ret = 0;
 
 	ret = pthread_mutex_lock(&at->mutex);
 	if (ret != 0) {
-		printf("加锁出错");
-		strerror(ret);
-		return (void*)-1;
+		printf("thread lock:%s\n", strerror(ret));
+		return (NULL);
 	}
-	for (int i = 0; i <at->length; i++) {
+	puts("加锁成功");
+	for (int i = 0; i < at->length; i++) {
 		at->array[i]++;
 		printf("%d ", at->array[i]);
 	}
 	printf("\n");
-	sleep(5);
+	sleep(4);
+	pthread_mutex_unlock(&at->mutex);
+	puts("解锁成功");
 
-	ret = pthread_mutex_unlock(&at->mutex);
-	if (ret != 0) {
-		printf("解锁出错");
-		strerror(ret);
-		return (void*)-1;
-	}
-	return argv;
+	return (argv);
 }
 
+void *function1(void *argv) {
+	array_t *at = argv;
+	int ret = 0;
 
-	
+	sleep(4);
+	ret = pthread_mutex_trylock(&at->mutex);
+	if (ret == 0) {
+	 	puts("加锁成功");
+		for (int i = 0; i < at->length; i++) {
+			at->array[i]++;
+			printf("%d ", at->array[i]);
+		}
+		printf("\n");
 
+		pthread_mutex_unlock(&at->mutex);
+		puts("解锁成功");
+	}
+	else
+		printf("th3:%s\n", strerror(ret));
 
-	
-	
+	return argv;
+}
+//数组的操作函数
+//获取数组的长度
+static inline int get_length(const void *array_p) {
+	array_t *at = (array_t*)array_p;
+	int length_t = 0;
+
+	pthread_mutex_lock(&at->mutex);
+	length_t = at->length;
+	pthread_mutex_unlock(&at->mutex);
+
+	return (length_t);
+}
+
+//根据索引获取数组中的数值
+static inline int get_element(const void *array_p, const int index) {
+	array_t *at = (array_t*)array_p;
+	int retval = 0;
+
+	pthread_mutex_lock(&at->mutex);
+	if (index >= at->length) {
+		pthread_mutex_unlock(&at->mutex);
+		retval = -1;
+		errno = EINVAL;
+	}
+	else {
+		retval = at->array[index];
+		pthread_mutex_unlock(&at->mutex);
+	}
+
+	return (retval);
+}
+
+//修改数组中的单个值
+static inline int set_element(const void *array_p, const int index, int new_value) {
+	array_t *at = (array_t*)array_p;
+	int retval = 0;
+
+	pthread_mutex_lock(&at->mutex);
+	if (index >= at->length) {
+		pthread_mutex_unlock(&at->mutex);
+		retval = -1;
+		errno = EINVAL;
+	}
+	else {
+		at->array[index] = new_value;
+		pthread_mutex_unlock(&at->mutex);
+	}
+	return (retval);
+}
+
+//修改数组中的多个值
+static inline int set_elements(const void *array_p, void *(*oper_elements)(void *), void *argv) {
+	array_t *at = (array_t*)array_p;
+	int *ret = NULL, retval = 0;
+
+	pthread_mutex_lock(&at->mutex);
+	ret = oper_elements(argv);
+	if (*ret == -1) {
+		if (errno != 0) {
+			perror("set_elements");
+			retval = -1;
+		}
+	}
+	pthread_mutex_unlock(&at->mutex);
+
+	return (retval);
+}
+
 
